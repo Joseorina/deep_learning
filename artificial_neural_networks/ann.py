bd7bc98c691177153f07e2ce725667c84bf0eb2a
Joseorina <orinangare@gmail.com>
2019-11-19 06:11:09 +0300

    Artificial Neural Networks

--- artificial_neural_networks/ann.py
+++ artificial_neural_networks/ann.py
@@ -1,8 +1,34 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 
-dataset = pd.read_csv(abs_file_path)
-X = dataset.iloc[:, 3:13].values
-y = dataset.iloc[:, 13].values
+dataset =pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:,3:13].values
+y = dataset.iloc[:,13].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+label_encoder_X_1 = LabelEncoder()
+X[:,1]=label_encoder_X_1.fit_transform(X[:,1])
+
+label_encoder_X_2 = LabelEncoder()
+X[:,2] = label_encoder_X_2.fit_transform(X[:,2])
+
+onehotencoder = OneHotEncoder(categorical_features=[1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Making the ANN
+from keras.models import Sequential
+from keras.layers import Dense
+
+classifier = Sequential()
