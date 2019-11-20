ea1ee1dc620ed40022fd00e2a8af2e17dbc9eea3
Joseorina <orinangare@gmail.com>
2019-11-20 04:25:13 +0300

    ANN

--- artificial_neural_networks/ann.py
+++ artificial_neural_networks/ann.py
@@ -52,3 +52,6 @@ cm = confusion_matrix(y_test, y_pred)
 # Predicting a new observation
 new_prediction = classifier.predict(sc.transform(np.array([[0.0,0,600,1,40,3,60000,2,1,1,50000]])))
 new_prediction = (new_prediction > 0.5)
+
+# Part 4: Evaluating imprtoving and Tuning the ANN
+# Evaluating
