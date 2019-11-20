13911e2fd7de693f8898550c653c2004f821534a
Joseorina <orinangare@gmail.com>
2019-11-20 05:33:49 +0300

    Improving the ANN

--- artificial_neural_networks/ann.py
+++ artificial_neural_networks/ann.py
@@ -85,10 +85,19 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
-def build_classifier():
+def build_classifier(optimizer):
     classifier = Sequential()
     classifier.add(Dense(output_dim = 6, init='uniform',activation='relu', input_dim=11))
     classifier.add(Dense(output_dim=6, init='uniform',activation='relu'))
     classifier.add(Dense(output_dim =1, init='uniform',activation='sigmoid'))
-    classifier.compile(optimizer='adam', loss ='binary_crossentropy', metrics=['accuracy'])
+    classifier.compile(optimizer=optimizer, loss ='binary_crossentropy', metrics=['accuracy'])
     return classifier
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters ={'batch_size':[25, 32],
+             'nb_epoch',[100,500],
+             'optimizer':['adam','rmsprop']}
+
+grid_search = GridSearchCV(estimator=classifier,
+                           param_grid=parameters,
+                           scoring='accuracy',
+                           cv=10)
