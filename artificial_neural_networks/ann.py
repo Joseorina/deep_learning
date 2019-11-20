27b7759b7478436587cf85511c44b4f37308d997
Joseorina <orinangare@gmail.com>
2019-11-20 05:31:04 +0300

    Improving the ANN

--- artificial_neural_networks/ann.py
+++ artificial_neural_networks/ann.py
@@ -81,3 +81,14 @@ variance = accuracies.std()
 
 
 # Tuning The ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(output_dim = 6, init='uniform',activation='relu', input_dim=11))
+    classifier.add(Dense(output_dim=6, init='uniform',activation='relu'))
+    classifier.add(Dense(output_dim =1, init='uniform',activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss ='binary_crossentropy', metrics=['accuracy'])
+    return classifier
