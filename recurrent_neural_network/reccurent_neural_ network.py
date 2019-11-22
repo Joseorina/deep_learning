5afcc14605df70662a7c167542b083a2ad404a8c
Joseorina <orinangare@gmail.com>
2019-11-22 13:22:31 +0300

    Part 1: Data Preprocessing

--- recurrent_neural_network/reccurent_neural_ network.py
+++ recurrent_neural_network/reccurent_neural_ network.py
@@ -1,2 +1,12 @@
-# -*- coding: utf-8 -*-
-#RNN
+# Recurrent Neural Network
+
+# Part 1 : Data Preprocessing
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset_train = pd.read_csv('dataset/Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:,1:2].values
+# Part 2 : BUilding the RNN
+
+#Part 3 :  MAking the predictions and visualizing the results
