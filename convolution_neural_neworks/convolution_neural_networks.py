41dfe012a5224cc1705978bda2a9bd34f55f1ac9
Joseorina <orinangare@gmail.com>
2019-11-22 04:45:46 +0300

    Making New Predictions

--- convolution_neural_neworks/convolution_neural_networks.py
+++ convolution_neural_neworks/convolution_neural_networks.py
@@ -64,4 +64,10 @@ from keras.preprocessing import image
 test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64,64,3))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-classifier.predict(test_image)
+result = classifier.predict(test_image)
+training_set.class_indices
+
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
